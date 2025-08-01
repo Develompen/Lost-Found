6fa67aaa53422f1bd168fbb7ab5906df39105a89
Асадбек <azizbekalibiev@gmail.com>
2025-08-01 22:01:15 +0500

    Исправил роутер для GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './assets/style/main.css';
 import { ItemsProvider } from './context/ItemsContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<ItemsProvider>
-                <App />
-            </ItemsProvider>
-		</BrowserRouter>
+		<HashRouter>
+			<BrowserRouter>
+				<ItemsProvider>
+					<App />
+				</ItemsProvider>
+			</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 );
