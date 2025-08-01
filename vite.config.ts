439abe1e3abe658d2c97d68f84be73502492964b
Асадбек <azizbekalibiev@gmail.com>
2025-08-01 22:38:06 +0500

    dqdqwd

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	base: '/lost-and-found/',
+	base: '/Lost-Found/',
 	plugins: [react()],
 });
